fa354e1715019b69be27f1452d29e1c04679c432
Dallas Womack <dallasawomack@gmail.com>
2025-05-14 22:50:27 -0700

    feat(encryption):implement rsa OAEP encryption

--- factory/rsa-factory.go
+++ factory/rsa-factory.go
@@ -1,6 +1,14 @@
 package factory
 
-import "github.com/DallasWmk/security_suite/product"
+import (
+	"bytes"
+	"crypto/rand"
+	"crypto/rsa"
+	"encoding/gob"
+	"fmt"
+
+	"github.com/DallasWmk/security_suite/product"
+)
 
 type RSAFactory struct{}
 
@@ -9,6 +17,19 @@ func (b *RSAFactory) CreateEncryptor() product.Encryption {
 }
 
 func (b *RSAFactory) GenerateKey() ([]byte, error) {
-	// TODO
-	return []byte{}, nil
+	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return []byte{}, fmt.Errorf("error generating RSA key: %s", err)
+	}
+
+	var buffer bytes.Buffer
+	encoder := gob.NewEncoder(&buffer)
+
+	err = encoder.Encode(privateKey)
+	if err != nil {
+		return []byte{}, fmt.Errorf("encoding error: %w", err)
+	}
+
+	bytes := buffer.Bytes()
+	return bytes, nil
 }
--- product/rsa-encryption.go
+++ product/rsa-encryption.go
@@ -1,13 +1,56 @@
 package product
 
+import (
+	"bytes"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha256"
+	"encoding/gob"
+	"fmt"
+)
+
 type RSAEncryptor struct{}
 
 func (e *RSAEncryptor) Encrypt(key []byte, data string) ([]byte, error) {
-	// TODO
-	return []byte{}, nil
+	privKey, err := decodePrivKey(key)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	label := []byte("message")
+
+	rng := rand.Reader
+	msgBytes := []byte(data)
+
+	ciphertext, err := rsa.EncryptOAEP(sha256.New(), rng, &privKey.PublicKey, msgBytes, label)
+	if err != nil {
+		return []byte{}, fmt.Errorf("error from encryption: %w", err)
+	}
+
+	return ciphertext, nil
 }
 
 func (e *RSAEncryptor) Decrypt(key []byte, encryptedData []byte) ([]byte, error) {
-	// TODO
-	return []byte{}, nil
+	privKey, err := decodePrivKey(key)
+	if err != nil {
+		return []byte{}, err
+	}
+	label := []byte("message")
+
+	plaintext, err := rsa.DecryptOAEP(sha256.New(), nil, privKey, encryptedData, label)
+	if err != nil {
+		return []byte{}, fmt.Errorf("error from decryption: %w", err)
+	}
+	return plaintext, nil
+}
+
+func decodePrivKey(key []byte) (*rsa.PrivateKey, error) {
+	privKey := rsa.PrivateKey{}
+	buffer := bytes.NewBuffer(key)
+	decoder := gob.NewDecoder(buffer)
+	err := decoder.Decode(&privKey)
+	if err != nil {
+		return nil, fmt.Errorf("decoding error: %w", err)
+	}
+	return &privKey, nil
 }
